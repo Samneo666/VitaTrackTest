fa2f17a8e87f42627366ead61b9a5fcedd378833
Sam <britpop1992@gmail.com>
2026-01-18 15:53:11 +0800

    改了文字

--- src/main/java/sam.java
+++ src/main/java/sam.java
@@ -2,5 +2,8 @@
 public class sam {
 	public static void main(String[] args) {
 		System.out.println("str== str2，罰款1000元!");
+		for (String string : args) {
+			System.out.println("啊!!!!!");
+		}
 	}
 }
