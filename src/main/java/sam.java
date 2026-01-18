b91b138f93d301857a77775f7c07b3abc06998f9
Sam <britpop1992@gmail.com>
2026-01-18 15:43:16 +0800

    no message

--- src/main/java/sam.java
+++ src/main/java/sam.java
@@ -1,7 +1,6 @@
 
-
 public class sam {
- public static void main(String[] args) {
-	System.out.println("str== str2，罰款1000元!");
-}
+	public static void main(String[] args) {
+		System.out.println("str== str2，罰款1000元!");
+	}
 }
