f4d78c59e8f13f3b3ca879c8151e4733d07681db
Sam <britpop1992@gmail.com>
2026-01-18 16:02:57 +0800

    加了package! 改了文字!

---
+++ src/main/java/OurProject/sam.java
@@ -0,0 +1,11 @@
+package OurProject;
+
+public class sam {
+	public static void main(String[] args) {
+		String str = "str== str2，罰款1000元!";
+		System.out.println(str);
+		for (String string : args) {
+			System.out.println("啊啊啊!!!!!");
+		}
+	}
+}
