93f02e59adb07cb03942f95f636b6f36666fd004
Sam <britpop1992@gmail.com>
2026-01-18 21:54:06 +0800

    預祝我們專題順利!

--- src/main/java/sam.java
+++ src/main/java/sam.java
@@ -2,8 +2,6 @@
 public class sam {
 	public static void main(String[] args) {
 		System.out.println("str== str2，罰款1000元!");
-		for (String string : args) {
-			System.out.println("啊啊啊啊啊啊!!!!!");
-		}
+		System.out.println("預祝我們專案順利!");
 	}
 }
