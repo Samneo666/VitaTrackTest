9863a4bc721a73ea0aa87e3f8fc40d699343e4f9
Sam <britpop1992@gmail.com>
2026-01-18 22:39:56 +0800

    加了文字!

--- src/main/java/sam.java
+++ src/main/java/sam.java
@@ -2,6 +2,7 @@
 public class sam {
 	public static void main(String[] args) {
 		System.out.println("str== str2，罰款1000元!");
-		System.out.println("預祝我們專案順利!");
+		System.out.println("預祝我們專題順利!");
+		System.out.println("我一定辦的到的!");
 	}
 }
