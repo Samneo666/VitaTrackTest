de816e46b095cdec90312a3a17d928e4568f908a
Sam <britpop1992@gmail.com>
2026-01-18 21:41:47 +0800

    加了一個啊

--- src/main/java/sam.java
+++ src/main/java/sam.java
@@ -3,7 +3,7 @@ public class sam {
 	public static void main(String[] args) {
 		System.out.println("str== str2，罰款1000元!");
 		for (String string : args) {
-			System.out.println("啊啊啊啊!!!!!");
+			System.out.println("啊啊啊啊啊啊!!!!!");
 		}
 	}
 }
